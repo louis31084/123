3393461a5d5ba8c1d643bcf426fb6500e5fc14d6
louis <louis31084@gmail.com>
2021-06-01 15:31:12 +0800

    123

--- src/main/java/box/BOX.java
+++ src/main/java/box/BOX.java
@@ -7,11 +7,11 @@ public class BOX {
         B1 box1 = new B1();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter object\'s length: ");
+        System.out.println("Please enter object's length: ");
         int length = Integer.parseInt(scanner.next());
-        System.out.println("Please enter object\'s width: ");
+        System.out.println("Please enter object's width: ");
         int width = Integer.parseInt(scanner.next());
-        System.out.println("Please enter object\'s height: ");
+        System.out.println("Please enter object's height: ");
         int height = Integer.parseInt(scanner.next());
         System.out.println("Length: " + length + ", width: " + width + ", height: " + height);
         if (box1.validate(length, width, height)) {
